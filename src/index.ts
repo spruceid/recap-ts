c2580ac13b3dd7aa8b06e7337b3efdd6bb22c5ec
chunningham <c.a.cunningham6@gmail.com>
2023-04-08 01:31:38 +1000

    initial implementation

---
+++ src/index.ts
@@ -0,0 +1,121 @@
+import { CID } from 'multiformats/cid';
+import { base58btc } from 'multiformats/bases/base58';
+import { base64url } from 'multiformats/bases/base64';
+
+export type PlainJSON = boolean | number | string | { [key: string]: PlainJSON } | { [key: number]: PlainJSON } | Array<PlainJSON>;
+export type AttObj = { [key: string]: { [key: string]: Array<PlainJSON> } };
+
+export class Recap {
+    #prf: Array<CID>;
+    #att: AttObj;
+
+    constructor(att: AttObj = {}, prf: Array<CID> = []) {
+        this.#prf = prf;
+        this.#att = att;
+    }
+
+    get proofs(): Array<CID> {
+        return this.#prf;
+    }
+
+    get attenuations(): AttObj {
+        return this.#att;
+    }
+
+    addProof(cid: string | CID) {
+        if (typeof cid === 'string') {
+            this.#prf.push(CID.parse(cid))
+        } else {
+            this.#prf.push(cid);
+        }
+    }
+
+    addAttenuation(resource: string, namespace: string = '*', name: string = '*', restriction?: PlainJSON) {
+        if (!validString(namespace) || namespace === '*') {
+            throw new Error('Invalid namespace');
+        }
+        if (!validString(name)) {
+            throw new Error('Invalid name');
+        }
+
+        const abString = `${namespace}/${name}`;
+        const ex = this.#att[resource];
+
+        if (ex !== undefined) {
+            if (restriction !== undefined) {
+                ex[abString].push(restriction);
+            }
+        } else {
+            this.#att[resource] = { [abString]: restriction ? [restriction] : [] };
+        }
+    }
+
+    static decode(recap: string): Recap {
+        if (!recap.startsWith(urnRecapPrefix)) {
+            throw new Error('Invalid recap urn');
+        }
+
+        const { att, prf } = decodeRecap(recap.slice(urnRecapPrefix.length));
+        return new Recap(att, prf)
+    }
+
+    encode(): string {
+        return `${urnRecapPrefix}${encodeRecap(this.#att, this.#prf)}`
+    }
+}
+
+const urnRecapPrefix = 'urn:recap:';
+const stringRegex = /^[a-zA-Z0-9.*_+-]$/g;
+const abilityStringRegex = /^[a-zA-Z0-9.*_+-]\/[a-zA-Z0-9.*_+-]$/g;
+
+const validString = (str: string) =>
+    str.match(stringRegex) !== null
+
+const validAbString = (str: string) =>
+    str.match(abilityStringRegex) !== null
+
+const encodeRecap = (att: AttObj, prf: Array<CID>) => base64url.encode(new TextEncoder().encode(JSON.stringify({
+    att: orderObject(att),
+    prf: prf.map(cid => cid.toV1().toString(base58btc.encoder))
+})))
+
+const decodeRecap = (recap: string): { att: AttObj, prf: Array<CID> } => {
+    const { att, prf } = JSON.parse(new TextDecoder().decode(base64url.decode(recap)));
+
+    // TODO ensure the att keys are valid URIs
+    // because URIs are so broad, there's no easy/efficient way to do this
+    for (const ob of Object.values(att)) {
+        if (!(ob instanceof Object)) {
+            throw new Error('Invalid attenuation object');
+        }
+        for (const ab of Object.keys(ob)) {
+            if (!validAbString(ab)) {
+                throw new Error('Invalid ability string');
+            }
+        }
+    }
+
+    if (!isSorted(att)) {
+        throw new Error('Attenuation object is not properly sorted');
+    }
+
+    return { att, prf: prf.map((cid: string) => CID.parse(cid)) }
+}
+
+const isSorted = (obj: PlainJSON) => {
+    if (obj instanceof Array) {
+        return obj.every(isSorted)
+    } else if (obj instanceof Object) {
+        return Object.keys(obj) === Object.keys(obj).sort() && Object.values(obj).every(isSorted)
+    } else {
+        true
+    }
+}
+
+const orderObject = (obj: PlainJSON) =>
+    Object.keys(obj).sort().reduce((sorted, key) => {
+        const value = obj[key];
+        sorted[key] = value instanceof Array ? value.map(orderObject)
+            : value instanceof Object ? orderObject(value) : obj[key];
+        return sorted
+    }, {})
