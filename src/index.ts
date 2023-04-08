04e79f6795560e3147bfa0e6e9e838d649c130a6
chunningham <c.a.cunningham6@gmail.com>
2023-04-08 21:19:02 +1000

    move misc stuff to ./utils

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { CID } from 'multiformats/cid';
-import { base58btc } from 'multiformats/bases/base58';
-import { base64url } from 'multiformats/bases/base64';
+import {
+    AttObj,
+    PlainJSON,
+    orderObject,
+    encodeRecap,
+    decodeRecap,
+    validString
+} from './utils';
 
-export type PlainJSON = boolean | number | string | { [key: string]: PlainJSON } | { [key: number]: PlainJSON } | Array<PlainJSON>;
-export type AttObj = { [key: string]: { [key: string]: Array<PlainJSON> } };
+export { AttObj, PlainJSON, CID };
+const urnRecapPrefix = 'urn:recap:';
 
 export class Recap {
     #prf: Array<CID>;
@@ -38,7 +44,7 @@ export class Recap {
                 return acc;
             }, {}) as { [key: string]: Array<string> };
             for (const [namespace, names] of Object.entries(resourceAbilities)) {
-                statement += `(${section}) "${namespace}": ${names.map((n: string) => '"' + n + '"').join(', ')} for "${resource}". `;
+                statement += `(${section}) "${namespace}": ${names.map(n => '"' + n + '"').join(', ')} for "${resource}". `;
                 section += 1;
             }
         }
@@ -87,62 +93,3 @@ export class Recap {
         return `${urnRecapPrefix}${encodeRecap(this.#att, this.#prf)}`
     }
 }
-
-const urnRecapPrefix = 'urn:recap:';
-const stringRegex = /^[a-zA-Z0-9.*_+-]$/g;
-const abilityStringRegex = /^[a-zA-Z0-9.*_+-]\/[a-zA-Z0-9.*_+-]$/g;
-
-const validString = (str: string) =>
-    str.match(stringRegex) !== null
-
-const validAbString = (str: string) =>
-    str.match(abilityStringRegex) !== null
-
-const encodeRecap = (att: AttObj, prf: Array<CID>) => base64url.encode(new TextEncoder().encode(JSON.stringify({
-    att: orderObject(att),
-    prf: prf.map(cid => cid.toV1().toString(base58btc.encoder))
-})))
-
-const decodeRecap = (recap: string): { att: AttObj, prf: Array<CID> } => {
-    const { att, prf } = JSON.parse(new TextDecoder().decode(base64url.decode(recap)));
-
-    // TODO ensure the att keys are valid URIs
-    // because URIs are so broad, there's no easy/efficient way to do this
-    for (const ob of Object.values(att)) {
-        if (!(ob instanceof Object)) {
-            throw new Error('Invalid attenuation object');
-        }
-        for (const ab of Object.keys(ob)) {
-            if (!validAbString(ab)) {
-                throw new Error('Invalid ability string');
-            }
-        }
-    }
-
-    if (!isSorted(att)) {
-        throw new Error('Attenuation object is not properly sorted');
-    }
-
-    return { att, prf: prf.map((cid: string) => CID.parse(cid)) }
-}
-
-const isSorted = (obj: PlainJSON): boolean => {
-    if (obj instanceof Array) {
-        // its an array
-        return obj.every(isSorted)
-    } else if (obj instanceof Object) {
-        // its an object
-        return Object.keys(obj) === Object.keys(obj).sort() && Object.values(obj).every(isSorted)
-    } else {
-        // its a primitive
-        return true
-    }
-}
-
-const orderObject = (obj: PlainJSON): PlainJSON =>
-    Object.keys(obj).sort().reduce((sorted, key) => {
-        const value = obj[key];
-        sorted[key] = value instanceof Array ? value.map(orderObject)
-            : value instanceof Object ? orderObject(value) : obj[key];
-        return sorted
-    }, {})
---
+++ src/utils.ts
@@ -0,0 +1,64 @@
+import { base58btc } from 'multiformats/bases/base58';
+import { base64url } from 'multiformats/bases/base64';
+import { CID } from 'multiformats/cid';
+
+export type PlainJSON = boolean | number | string | { [key: string]: PlainJSON } | { [key: number]: PlainJSON } | Array<PlainJSON>;
+export type AttObj = { [key: string]: { [key: string]: Array<PlainJSON> } };
+
+const stringRegex = /^[a-zA-Z0-9.*_+-]$/g;
+const abilityStringRegex = /^[a-zA-Z0-9.*_+-]\/[a-zA-Z0-9.*_+-]$/g;
+
+export const validString = (str: string) =>
+    str.match(stringRegex) !== null
+
+export const validAbString = (str: string) =>
+    str.match(abilityStringRegex) !== null
+
+export const encodeRecap = (att: AttObj, prf: Array<CID>) => base64url.encode(new TextEncoder().encode(JSON.stringify({
+    att: orderObject(att),
+    prf: prf.map(cid => cid.toV1().toString(base58btc.encoder))
+})))
+
+export const decodeRecap = (recap: string): { att: AttObj, prf: Array<CID> } => {
+    const { att, prf } = JSON.parse(new TextDecoder().decode(base64url.decode(recap)));
+
+    // TODO ensure the att keys are valid URIs
+    // because URIs are so broad, there's no easy/efficient way to do this
+    for (const ob of Object.values(att)) {
+        if (!(ob instanceof Object)) {
+            throw new Error('Invalid attenuation object');
+        }
+        for (const ab of Object.keys(ob)) {
+            if (!validAbString(ab)) {
+                throw new Error('Invalid ability string');
+            }
+        }
+    }
+
+    if (!isSorted(att)) {
+        throw new Error('Attenuation object is not properly sorted');
+    }
+
+    return { att, prf: prf.map((cid: string) => CID.parse(cid)) }
+}
+
+export const isSorted = (obj: PlainJSON): boolean => {
+    if (obj instanceof Array) {
+        // its an array
+        return obj.every(isSorted)
+    } else if (obj instanceof Object) {
+        // its an object
+        return Object.keys(obj) === Object.keys(obj).sort() && Object.values(obj).every(isSorted)
+    } else {
+        // its a primitive
+        return true
+    }
+}
+
+export const orderObject = (obj: PlainJSON): PlainJSON =>
+    Object.keys(obj).sort().reduce((sorted, key) => {
+        const value = obj[key];
+        sorted[key] = value instanceof Array ? value.map(orderObject)
+            : value instanceof Object ? orderObject(value) : obj[key];
+        return sorted
+    }, {})
